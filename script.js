aea5740ad96d846b81c46ec86f13eeaf8a8ec05b
Maueen-Njango <maureennjangombira@gmail.com>
2024-03-21 22:14:32 +0300

    add changes

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
 
+//Initializing variable to store current display
+let currentDisplay = "0";
+let resultDisplay = false;
+//function to append the value to the current display
 
-const operators = prompt('Enter operator to perform the calculation(either +,-,* or/):');
+function appendToDisplay(value){
+    if (currentDisplay === "0" || resultDisplay){
 
-//const number1 = parseFloat(prompt('Enter the first number:'));
-//const number2 = parseFloat(prompt('Enter the second number:'));
+    
+}else{
+    currentDisplay += value;
+}
+resultDisplay = false;
+updateDisplay();
+}
+///function to update  the calculator display with the current content
+function updateDisplay(){
+    const displayElement = documents.getElementById("display");
+    displayElement.textContent = currentDisplay;
 
-if(operators=='+'){
-    result = number1+number2;
 }
-else if(operators=='-'){
-    result=number1-number2
+///function to culculate and display the result
+function calculateResults(){
+    const result = eval(currentDisplay)
+    //apend the results to the current display preceded by an equal sign
+    currentDisplay += "\n=" + result.toString();
+
+//update the culculator display with results
+updateDisplay();
+} expected(error) {
+    currentDisplay += "\nError";
+    updateDisplay();
 }
-else if(operators=='*'){
-    result=number1*number2
+resultDisplay = true;
+
+
+///function to clear the last element from the current display
+
+function clearLastElement(){
+    currentDisplay = currentDisplay.slice(0, -1)
+    //if the current display becomes empty, set it back to 0
+    if (currentDisplay ==="") {
+        currentDisplay = "0";
+    }
+
+//update the calculator display to reflect the changes
+
+updateDisplay();
 }
-else {
-    result=number1/number2
+
+///function to clear the entire display
+function clearDisplay(){
+    currentDisplay = "0";
+    
+    updateDisplay();
 }
-window.alert("Results is"+results);
+window.addEventListener("resize", handleOverflow);
+handleOverflow;
