3aa86ef67e0e6021a0686b30ef4ee59aa31b4b47
Maueen-Njango <maureennjangombira@gmail.com>
2024-03-27 23:06:20 +0300

    add changes

--- script.js
+++ script.js
@@ -1,35 +1,89 @@
-function appendToDisplay(value){
-    const element = document.getElementById("display");
-    if (element == null) return; // Should never happen, but just in case...
-    if (element.textContent ==="0"){
-        element.textContent = value;
+var leftVar = null;
+var rightVar = null;
+var opp = null;
+
+
+function handleInput(element) {
+    const operations = ['+', '-', '*', '/'];
+    const documentElement = document.getElementById("display");
+    if (operation in operations) {
+        opp = operation;
+        // if leftVar is Null,  set it to the current value of element.value and make rightVar Null
+        if (leftVar == null) {
+            leftVar = Number(documentElement.textContent);
+        } else if (rightVar == null) {
+            rightVar = Number(documentElement.textContent);
+        }
+        if (operation === "+") {
+            result = add(num1, num2);
+        } else if (operation === "-") {
+            result = subtract(num1, num2);
+        } else if (operation === "*") {
+            result = multiply(num1, num2);
+        } else if (operation === "/") {
+            result = divide(num1, num2);
+        } else {
+            // if leftVar is null, store
+        }
     } else {
-       element.textContent += value;
-    
+        // append document content
+        document.getElementById("display").textContent += value;
     }
 }
-function clearlastElement(){
+
+
+function appendToDisplay(value) {
     const element = document.getElementById("display");
-    const content = element.textContent;
-    if (content.length == 0) return;
-    element.textContent = element.textContent.substring(0, display.length -1);
+    if (element == null) return; // Should never happen, but just in case...
+    if (element.textContent === "0") {
+        element.textContent = value;
+    } else {
+        // handleInput(value); todo, finish implementing the function (with BODMAS in consideration)
+        element.textContent += value;
+    }
+}
+
+function clearlastElement() {
+    const element = document.getElementById("display");
+    const content = element.textContent;// context of the calculator
+    if (content.length == 0) return;// check whether the content is empty
+    element.textContent = element.textContent.substring(0, display.length - 1);//to extract characters btwm the two strings
     if (content.length == 1) {
         element.textContent = "0";
     } else {
         // remove the last character of content
-        element.textContent = content.slice(0,-1);
+        element.textContent = content.slice(0, -1);
     }
 
 
 }
-function clearDisplay(){
+function clearDisplay() {
     const element = document.getElementById("display");
     const content = element.textContent;
-    element.textContent = element.textContent.substring(0, display.length -1);
+    element.textContent = element.textContent.substring(0, display.length - 1);
     if (content.length == 1) {
         element.textContent = "0";
 
 
+    }
 }
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    //  Check for division by zero error
+    // for now, return zero. in future, through an exception or error message
+    if (b === 0) return 0;
+    return a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
 }
 
